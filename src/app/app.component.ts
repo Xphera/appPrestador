f439ce85fb00398c79fe7c2c20b8f1135a8e0497
oscarruiz86@gmail.com <oscarruiz86@gmail.com>
2018-07-07 08:17:38 -0500

    commint inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, ModalController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AutenticacionProvider } from '../providers/autenticacion/autenticacion';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = 'LoginPage';
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    modalCtrl: ModalController,
+    _autenticacionPrvdr:AutenticacionProvider
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+      _autenticacionPrvdr.activo()
+      .then((data)=>{
+        if(data["data"] == null){
+          this.rootPage = 'LoginPage'
+        }else{
+          this.rootPage= 'TabsPage';
+        }
+        splashScreen.hide();
+      })
+
     });
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,47 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { PeticionProvider } from '../providers/peticion/peticion';
+import { SesionProvider } from '../providers/sesion/sesion';
+
+import {HttpClientModule} from '@angular/common/http';
+import { IonicComponentProvider } from '../providers/ionic-component/ionic-component';
+import { DisponibilidadProvider } from '../providers/disponibilidad/disponibilidad';
+import { ZonaProvider } from '../providers/zona/zona';
+import { AutenticacionProvider } from '../providers/autenticacion/autenticacion';
+import { AlmacenamientoProvider } from '../providers/almacenamiento/almacenamiento';
+
+import { NativeStorage } from '@ionic-native/native-storage';
+import { PrestadorProvider } from '../providers/prestador/prestador';
+import { LocalizarUbicacionProvider } from '../providers/localizar-ubicacion/localizar-ubicacion';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PeticionProvider,
+    SesionProvider,
+    IonicComponentProvider,
+    DisponibilidadProvider,
+    ZonaProvider,
+    AutenticacionProvider,
+    AlmacenamientoProvider,
+    NativeStorage,
+    PrestadorProvider,
+    LocalizarUbicacionProvider
   ]
 })
 export class AppModule {}
